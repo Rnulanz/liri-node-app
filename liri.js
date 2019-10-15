eaa4f0a6e9cee2f0351774c18fadba7e00b5668e
Dick James <rnulanz@gmail.com>
2019-10-15 19:35:43 -0400

    Update

--- liri.js
+++ liri.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 
-// var keys = require('./keys.js');
+var keys = require('./keys.js');
 var inquirer = require('inquirer');
 var moment = require('moment');
 moment().format();
 var fs = require('fs');
 var axios = require('axios');
-// var Spotify = ('node-spotify-api');
-// var spotify = new Spotify(keys.spotify);
+var Spotify = require('node-spotify-api');
+var spotifyObject = new Spotify(keys.spotify);
 
 
 inquirer
@@ -46,7 +46,7 @@ inquirer
                 axios.get(queryURL).then(function(response){
                     // console.log(response)
                     if(response.data.Error){
-                        console.log("Movie was not found sorry, look again or for something new.")
+                        console.log("Movie was not found sorry, look again or look for something new.")
                     }else if(response.data){
                         console.log(`Title: ${response.data.Title}`);
                         console.log(`Released: ${response.data.Released}`)
@@ -87,9 +87,10 @@ inquirer
             for(i = 0; i < response.data.length; i++){
                 let date = moment(response.data[i].datetime).format('MM/DD/YYYY')
                 console.log(`Venue name: ${response.data[i].venue.name}`);
-                console.log(`Lineup: ${response.data[i].venue.lineup}`);
+                console.log(`Lineup: ${response.data[i].lineup}`);
                 console.log(`Country: ${response.data[i].venue.country}`);
                 console.log(`${date}`);
+                console.log("----------------------------------------------------------")
             }
             fs.appendFile('log.txt', `\nConcert: ${artist}`, function(err){
                 if(err){
@@ -101,8 +102,37 @@ inquirer
         })
     }
 })   
+}else if(user.choice === 'spotify-this'){
+    console.log(`Hello ${user.username} lets get to rockin' out.`)
+    inquirer
+    .prompt([
+        {
+            type:"input",
+            message: "What song would you like to listen to?",
+            name: "track"
+        }
+    ])
+.then(function(result){
+    if(result.track=== ""){
+        console.log('No song was entered, please go back and enter a valid song.')
+    } else{
+        spotifyObject
+        .search({type: 'track', query: result.track})
+        .then(function(response){
+            console.log(response.tracks.items);
+            for(i = 0; i < response.tracks.items.length; i++){
+                console.log("\n===================================")
+                console.log(`Song:  ${response.tracks.items[i].name}`);
+                console.log(`Artist: ${response.tracks.items[i].album.artists[0].name}`);
+                console.log(`Spotify Preview: ${response.tracks.items[i].album.external_urls.spotify}`);
+                console.log(`Album: ${response.tracks.items[i].album.name}`);
+                console.log(`Release Year: ${response.tracks.items[i].album.release_date}`);
+                console.log(`Preview: ${response.tracks.items[i].preview_url}`);
+            }
+        })
+    }
+})    
 }
-// Start the next choice here
 });
     
 
